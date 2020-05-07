5731bb0ddc1dd1b999a776f475da8604dd7810bc
Edge2289 <1131191695@qq.com>
2020-05-07 22:09:55 +0800

    删除新闻的go

--- utils/news/news.go
+++ utils/news/news.go
@@ -1,10 +1,37 @@
-package _news
+package news
 
+import (
+	"errors"
+	"fmt"
+)
 
-type NewsStruct interface {
+type StructPush interface {
 	Push()
 }
 
+type Ambient struct {
+	Istruct string
+	//AmbientPush StructPush
+}
+
+func (n *Ambient) Travel() (string, error) {
+
+	fmt.Print(n.Istruct)
+	//switch n.Istruct {
+	//	case "sms":
+	//		Sms{}.Push()
+	//		return n.Istruct+" success", nil
+	//		break
+	//	case "email":
+	//		Email{}.Push()
+	//		return n.Istruct+" success", nil
+	//		break
+	//	default:
+	//		return "error", errors.New("not News struct ")
+	//		break
+	//}
+	return "error", errors.New("not News struct ")
+}
 /**
 获取需要使用的对象
  */
